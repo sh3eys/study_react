716b40f2e8f5ef3cea083aafcf79a30db552a61a
kkudou <sharaku3eyes@gmail.com>
2019-02-09 21:57:40 +0900

    [new] first deployed

---
+++ build/precache-manifest.197b2e8525acb0ee1e2691c21db30d3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3891a60974eade427694",
+    "url": "/static/js/main.3891a609.chunk.js"
+  },
+  {
+    "revision": "39ad5116a74f5b8e8fda",
+    "url": "/static/js/1.39ad5116.chunk.js"
+  },
+  {
+    "revision": "3891a60974eade427694",
+    "url": "/static/css/main.db353fde.chunk.css"
+  },
+  {
+    "revision": "dcfe724a46d18384286f56ea651e49a9",
+    "url": "/index.html"
+  }
+];
---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_OqxYVJu9we2myzOZbhjFbZvdwzW4YpA",
+  authDomain: "study-react-20fe1.firebaseapp.com",
+  databaseURL: "https://study-react-20fe1.firebaseio.com",
+  projectId: "study-react-20fe1",
+  storageBucket: "study-react-20fe1.appspot.com",
+  messagingSenderId: "850627636940"
+};
